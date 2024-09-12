4a84f8120f5ee7957b2725f9ba8080a4224ec4df
Conny <pedalconny@hotmail.com>
2024-09-12 15:59:08 +0200

    Add method addProduct in class Warehouse

--- src/main/java/service/Warehouse.java
+++ src/main/java/service/Warehouse.java
@@ -1,4 +1,21 @@
 package service;
+import entities.Product;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Warehouse {
+    private final List<Product> products = new ArrayList<>();
+
+    public void addProduct(Product product) {
+        if(product.name() == null || product.name().isEmpty()) {
+            throw new IllegalArgumentException("Product name cannot be empty!");
+        }
+
+        for(Product p : products) {
+            if(p.id() == product.id()) {
+                throw new IllegalArgumentException("Product with the same ID already exists!");
+            }
+        }
+    products.add(product);
+    }
 }
