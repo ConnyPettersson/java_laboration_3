a0ba47618782d684fa797a5085de769f6fd714c7
Conny <pedalconny@hotmail.com>
2024-09-12 15:26:18 +0200

    Add enum to warehouse

--- src/main/java/entities/Category.java
+++ src/main/java/entities/Category.java
@@ -1,6 +1,11 @@
 package entities;
 
-public class Category {
-
-
+public enum Category {
+    CYMBALS,
+    SNARE_DRUMS,
+    BASS_DRUMS,
+    STICKS,
+    PEDALS,
+    DRUM_HEADS,
+    TOM_TOMS
 }
