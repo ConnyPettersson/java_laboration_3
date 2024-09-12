9cc36aef9b1a28b1487ff6fa8091ee329760edd3
Conny <pedalconny@hotmail.com>
2024-09-12 19:44:32 +0200

    Added method for adding products manually

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,17 @@
 package org.example;
+import entities.Category;
+import entities.Product;
+import service.Warehouse;
+import java.time.LocalDate;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello There!");
+        Warehouse warehouse = new Warehouse();
+
+        Product product1 = new Product(1, "Zildjan", Category.CYMBALS, 7, LocalDate.now(), LocalDate.now());
+
+        warehouse.addProduct(product1);
+
+        System.out.println("Product added successfully");
     }
 }
