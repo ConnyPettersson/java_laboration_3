577909d2b62a9faa8fa835925bf8279bbcc46ec9
Conny <pedalconny@hotmail.com>
2024-10-06 09:28:49 +0200

    Add first HTTP-request POST in WarehouseResource

--- src/main/java/org/example/resource/WarehouseResource.java
+++ src/main/java/org/example/resource/WarehouseResource.java
@@ -1,4 +1,32 @@
 package org.example.resource;
 
+import entities.Category;
+import entities.Product;
+import service.WarehouseService;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.util.List;
+
+@Path("/warehouse")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class WarehouseResource {
+
+    @Inject
+    private WarehouseService warehouseService;
+
+    @POST
+    @Path("/products")
+    public Response addProduct(Product product) {
+        try {
+            warehouseService.addProduct(product);
+            return Response.status(Response.Status.CREATED).entity(product).build();
+        } catch (IllegalArgumentException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
+        }
+    }
 }
